a359c22ec81780cf2dd8d67628dc84f58c1dd8e0
Daniil Fedotov <dfedotov@pivotal.io>
2017-02-06 13:19:16 +0000

    SSL support

--- README.md
+++ README.md
@@ -22,11 +22,52 @@ Will list `.pem` files from the selected directory.
 To rebuild and run a release (requires Erlang to be installed):
 
 ```
-make run CERT_DIR="/my/cacert/directory"
+make run CERT_DIR="/my/cacert/directory" PORT=8080
 ```
 
 To run from the pre-built escript (requires Erlang to be installed):
 
 ```
-CERT_DIR="/my/cacert/directory" ./trust_store_http
+CERT_DIR="/my/cacert/directory" PORT=8080 ./trust_store_http
+```
+
+
+### HTTPS:
+
+To start an HTTPS server, you should provide ssl options. It can be done via
+Erlang `.config` file format:
+
+```
+[{trust_store_http,
+    [{ssl_options, [{cacertfile,"/path/to/testca/cacert.pem"},
+                    {certfile,"/path/to/server/cert.pem"},
+                    {keyfile,"/path/to/server/key.pem"},
+                    {verify,verify_peer},
+                    {fail_if_no_peer_cert,false}]}]}]
+```
+
+
+This configuration can be added to `rel/sys.config`
+if you're running the application from source `make run`
+
+Or it can be specified as an environment variable:
+
+```
+CERT_DIR="/my/cacert/directory" PORT=8443 CONFIG_FILE=my_config.config ./trust_store_http
+```
+
+Port and directory can be also set via config file:
+
+
+```
+[{trust_store_http,
+    [{directory, "/tmp/certs"},
+     {port, 8081},
+     {ssl_options, [{cacertfile,"/path/to/testca/cacert.pem"},
+                    {certfile,"/path/to/server/cert.pem"},
+                    {keyfile,"/path/to/server/key.pem"},
+                    {verify,verify_peer},
+                    {fail_if_no_peer_cert,false}]}]}]
+     }
+    }
 ```
--- src/trust_store_http_app.erl
+++ src/trust_store_http_app.erl
@@ -5,31 +5,71 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-    CurrentDir = get_default_dir(),
-    Directory = case application:get_env(trust_store_http, directory) of
-        undefined ->
-            application:set_env(trust_store_http, directory, CurrentDir),
-            CurrentDir;
-        Dir -> Dir
-    end,
+    init_config(),
+    Directory = get_directory(),
+    Port = get_port(),
     Dispatch = cowboy_router:compile([
         {'_', [{"/", list_handler, []},
-               {"/certs/[...]", cowboy_static, {dir, Directory, [{mimetypes, {<<"text">>, <<"html">>, []}}]}}]}
+               {"/certs/[...]", cowboy_static,
+                {dir, Directory, [{mimetypes, {<<"text">>, <<"html">>, []}}]}}]}
     ]),
-    cowboy:start_http(my_http_listener, 100, [{port, 8080}],
-        [{env, [{dispatch, Dispatch}]}]
-    ),
+    case get_ssl_options() of
+        undefined  -> start_http(Dispatch, Port);
+        SslOptions -> start_https(Dispatch, Port, SslOptions)
+    end,
     trust_store_http_sup:start_link().
 
 stop(_State) ->
-	ok.
+    ok.
 
+start_http(Dispatch, undefined) ->
+    start_http(Dispatch, 8080);
+start_http(Dispatch, Port) ->
+    {ok, _} = cowboy:start_http(my_http_listener, 100,
+                                [{port, Port}],
+                                [{env, [{dispatch, Dispatch}]}]).
 
+start_https(Dispatch, undefined, SslOptions) ->
+    start_https(Dispatch, 8443, SslOptions);
+start_https(Dispatch, Port, SslOptions) ->
+    {ok, _} = cowboy:start_https(https, 100,
+                                 [{port, Port}] ++ SslOptions,
+                                 [{env, [{dispatch, Dispatch}]}]).
 
-get_default_dir() ->
-    case os:getenv("CERT_DIR") of
+get_directory() ->
+    Dir = case os:getenv("CERT_DIR") of
         false ->
-            {ok, CurrentDir} = file:get_cwd(),
-            CurrentDir;
-        Dir -> Dir
+            case application:get_env(trust_store_http, directory, undefined) of
+                undefined ->
+                    {ok, CurrentDir} = file:get_cwd(),
+                    CurrentDir;
+                AppDir -> AppDir
+            end;
+        EnvDir -> EnvDir
+    end,
+    application:set_env(trust_store_http, directory, Dir),
+    Dir.
+
+get_ssl_options() ->
+    application:get_env(trust_store_http, ssl_options, undefined).
+
+get_port() ->
+    case os:getenv("PORT") of
+        false -> application:get_env(trust_store_http, port, undefined);
+        Port  -> list_to_integer(Port)
+    end.
+
+init_config() ->
+    case os:getenv("CONFIG_FILE") of
+        false -> ok;
+        ConfigFile ->
+            case file:consult(ConfigFile) of
+                {ok, [Config]} ->
+                    lists:foreach(fun({App, Conf}) ->
+                        [application:set_env(App, Key, Val)
+                         || {Key, Val} <- Conf]
+                    end,
+                    Config);
+                _ -> ok
+            end
     end.
